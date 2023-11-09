6bf270674936b06fedabb504b04e47d7b9b742eb
FajarFarisya <fajarfarisya@gmail.com>
2023-11-10 00:32:54 +0700

    feat(init): installation and setup laravel tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  ccontent: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+    fontFamily:{
+      sans:[
+        'Roboto'
+      ]
+    }
+  },
+  plugins: [],
+}
+
