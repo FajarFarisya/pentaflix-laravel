2b81fe5d9cb21925b6d1041994992618772c14e9
FajarFarisya <fajarfarisya@gmail.com>
2023-11-10 01:29:01 +0700

    feat(footer): create view footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- Footer -->
+<footer class="bg-black rounded-lg shadow m-4">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <hr class="my-6 border-gray-600 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#"
+                class="hover:underline">PentaFlix</a>. All Rights Reserved.</span>
+    </div>
+</footer>
+<!-- End Footer -->
